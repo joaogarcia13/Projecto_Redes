c0bb63b6fc517fae00d8cc1d7708a0534dddf243
Martiinsss <martins.m19@hotmail.com>
2022-11-14 10:49:37 +0000

    Agora vai

--- qos/qos.sh
+++ qos/qos.sh
@@ -5,11 +5,10 @@ ipsrc = $2
 ipdst = $3
 velocidadeDownload = $4
 velocidadeUpload = $5
+  
+sudo tc class add dev $interface parent 1:0 classid 1:1 hfsc sc rate $velocidadeDownload ul rate $velocidadeUpload #temos de ter cuidado com o que é introduzido
+sudo tc filter add dev $interface parent 1: protocol all u32 match ip src $ipsrc flowid 1:1
+sudo tc filter add dev $interface parent 1: protocol all u32 match ip dst $ipdst flowid 1:2
 
-iniciarQos() {
-    sudo tc qdisc add dev $interface root handle 1:0 
-}
-
-aplicarNovaRegra() {
-
-}
+#Para elimiar um filtro
+sudo tc filter del dev $interface parent 1:0
