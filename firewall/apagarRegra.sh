7fcc4fdd4c2d1c3981f43bb287715028c315f0f4
Martiinsss <martins.m19@hotmail.com>
2023-01-11 23:03:03 +0000

    erro nos scripts firewall: corrigido

--- firewall/apagarRegra.sh
+++ firewall/apagarRegra.sh
@@ -1,11 +1,11 @@
-if [$# -eq 0]
+#!/bin/bash
+
+if [ $2 = "null" ];
 then
     sudo ufw route delete deny in on $1 to any
+elif [ $2 = "port" ];
+then
+    sudo ufw route delete deny in on $1 to any port $3
 else
-    if [$2 == "port"]
-    then
-        sudo ufw route delete deny in on $1 to any port $3
-    else
-        sudo ufw route delete deny in on $1 to $3
-    fi
+    sudo ufw route delete deny in on $1 to $3
 fi
