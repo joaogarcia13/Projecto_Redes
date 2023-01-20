ad86ec90cfa51c4587665d4e558c24ce308ddf19
Martiinsss <martins.m19@hotmail.com>
2023-01-20 16:29:53 +0000

    firewall atualizado

--- firewall/iniciar.sh
+++ firewall/iniciar.sh
@@ -1,12 +1,10 @@
-#docker image build -t firewall .
-#docker run firewall
-
-#Se tiver a funcionar como router as rotas tem que estar a ser permitidas
 sudo ufw default allow routed
-#Prevenir ataques à máquina
 sudo ufw default deny incoming
 sudo ufw default deny outgoing
 sudo ufw allow ssh
 sudo ufw allow from any port ssh
 sudo ufw allow 5000
+sudo ufw allow 9100
+sudo ufw allow from any port 9100
+sudo ufw allow from any port 9090
 sudo ufw --force enable
