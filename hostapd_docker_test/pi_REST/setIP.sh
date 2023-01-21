e7b589ede5c22170b734ca514c230d8cf198ec14
joaogarcia13 <joaobernardogarcia@gmail.com>
2023-01-21 19:09:41 +0000

    bb

--- hostapd_docker_test/pi_REST/setIP.sh
+++ hostapd_docker_test/pi_REST/setIP.sh
@@ -13,9 +13,12 @@ subnet $2 netmask 255.255.255.0 {
 
 echo "ifconfig wlan0 $1/24 up
 #iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables-nft -t nat -C POSTROUTING -o eth0 -j MASQUERADE || iptables-nft -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables-nft -C FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT || iptables-nft -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables-nft -C FORWARD -i wlan0 -o eth0 -j ACCEPT || iptables-nft -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+iptables -t nat -C POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -C FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -C FORWARD -i wlan0 -o eth0 -j ACCEPT
+iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 dhcpd -cf dhcp.conf wlan0
 echo 'dhcp started'" > pi_REST/init-network.sh
 
