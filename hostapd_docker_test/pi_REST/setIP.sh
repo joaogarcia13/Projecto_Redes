90f459367c0bfd85d45196468a3cfa85144dbfbf
joaogarcia13 <joaobernardogarcia@gmail.com>
2023-01-21 18:37:41 +0000

    ini

--- hostapd_docker_test/pi_REST/setIP.sh
+++ hostapd_docker_test/pi_REST/setIP.sh
@@ -12,7 +12,10 @@ subnet $2 netmask 255.255.255.0 {
 " > dhcp.conf #adicionar a string as subnets
 
 echo "sudo ifconfig wlan0 $1/24 up
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables-nft -t nat -C POSTROUTING -o eth0 -j MASQUERADE && iptables-nft -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+iptables-nft -C FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT && iptables-nft -D FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables-nft -C FORWARD -i wlan0 -o eth0 -j ACCEPT && iptables-nft -D FORWARD -i wlan0 -o eth0 -j ACCEPT
 sudo dhcpd -cf dhcp.conf wlan0
 echo 'dhcp started'" > pi_REST/init-network.sh
 
